20a8ec14f56fb09d9b1d8e4c1a96d643de8e5cc7
Joseph Raymond <jraymond@ucdavis.edu>
2022-12-06 10:11:04 -0800

    path fixes

--- data load module.R
+++ data load module.R
@@ -1,13 +1,11 @@
+
 rm(list = ls())
 path2home <- "test"
 datapath <- "/home/akfin/"
 sub_dir <- "jraymond"
 main_dir <- paste(path2home, datapath, sep = "")
 
-#Run this once and then comment out
-dir.create(sub_dir)
-
-# check if sub directory exists 
+# check if sub directory exists and if it doesn't then create it.
 if (file.exists(sub_dir)){
   # specifying the working directory
   setwd(file.path(main_dir, sub_dir))
@@ -18,37 +16,29 @@ if (file.exists(sub_dir)){
   # specifying the working directory
   setwd(file.path(main_dir, sub_dir))
 }
-
-
-#install packages not installed in your environment, some are included here in anticipation of future needs
-packs <- c('readr', 'tidyverse', 'lubridate', 'leaflet', 'dplyr', 'gtools', 'ggplot2', 'sf', 'scales', 'ggmap',
-           'chron', 'zipcodeR', 'stringr','RColorBrewer', 'cowplot', 'ggcorrplot', "Hmisc")
+#loads my packages. If not installed in your environment, the missing ones gte installed
+packs <- c('readr', 'tidyverse', 'lubridate', 'leaflet', 'dplyr', 'gtools', 'ggplot2', 'sf', 'scales', 'ggmap','chron', 'zipcodeR', 'stringr','RColorBrewer','cowplot', 'ggcorrplot', "Hmisc")
+new.packages <- packs[!(packs %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 lapply(packs, require, character.only = T)
 
 
-maxyear=1980
-mminyear=2018
+maxyear <- 2021
+minyear <- 1991
 years <- minyear:maxyear
 
 
-load_data <- function(x){
+load_data <- function(xx){
   data_list <- list()
-  for (i in x){
-    temp_name <- paste(c("akfin_", x[i],"_database.csv"), collapse = "")#change this for different file name structures
-    temp_df <- read.csv(paste(c(path2home,datapath,temp), sep = ""))#get absolute path because the wd is one folder deeper (/home/akfin/jraymnond/)
-    data_list <- list.append(data_list, temp_df)
+  for (i in xx){
+    temp_name <- paste(c(as.character(i), " fish ticket data (revised 4-14-17)",".csv"), collapse = "")#change this for different file name structures
+    temp_df <- read.csv(paste(path2home,datapath,temp_name, sep = ""))#get absolute path because the wd is one folder deeper (/home/akfin/jraymnond/)
+    data_list <- append(data_list, temp_df)
+    print(paste(path2home,datapath,temp_name, sep = ""))
   }
   return(data_list)
 }
-load_data(years)
-
-for (i in 1:length(test)){
-  age <- c(age, nrow(test[[i]]))
-  tempfile <- commonpath +"year" +".csv"
-  df <- list()
-  temp <- read.csv(tempfile)
-  df <- df.append()
-}
-
+df <- load_data(years)
 
 rm(list = ls())
+#"ssh -L localhost:8989:localhost:3389 jraymond@makena.ucdavis.edu", and then connect to "localhost:8989"
