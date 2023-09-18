d644215ed01139c0921212b56343d2d056893cff
Joseph Raymond <jraymond@makena.ucdavis.edu>
2023-09-18 16:48:54 -0700

    Self-selection graphs and other

--- code/permit_cleaning.R
+++ code/permit_cleaning.R
@@ -1,4 +1,4 @@
-{packs <- c('readr', 'tidyverse', 'lubridate', 'leaflet', 'dplyr', 'gtools', 'ggplot2', 'sf', 'scales', 'ggmap','chron', 'zipcodeR', 'stringr','RColorBrewer','cowplot', 'ggcorrplot', "Hmisc", 'patchwork', 'xlsx', 'xtable', 'corrplot')
+{packs <- c('readr', 'tidyverse', 'lubridate', 'leaflet', 'dplyr', 'gtools', 'ggplot2', 'sf', 'scales', 'ggmap','chron', 'zipcodeR', 'stringr','RColorBrewer','cowplot', 'ggcorrplot', "Hmisc", 'patchwork', 'xlsx', 'xtable', 'corrplot', 'data.table')
 new.packages <- packs[!(packs %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 lapply(packs, require, character.only = T)}#probably could just use loadpacks but this should work
@@ -12,6 +12,11 @@ data_dir <- "./../../../"
 #want to load data filter it by salmon boats then store that year of data in a list
 datalist <- list.files(data_dir, pattern = "*.csv")
 permit_data <- list()
+
+library(readr)
+permit_clean <- read_csv("~/JoeData/clean_data/permit_clean.csv")
+save(permit_clean, file = "intermediate data/permit_clean.rdata")
+
 j <- 1
 for (i in datalist[1:length(datalist)]) {#15 starts at 2005
   temp <- read.csv(file.path(paste0(data_dir, i))) %>%
@@ -58,8 +63,108 @@ df <- myfiles[[1]]
 for(i in 2:length(myfiles)){
   df <- rbind(df, myfiles[[i]]) 
 }
-sum(is.na(df$Zip.Code))
-rm(myfiles)
+
+
+
+
+
+library(readr)
+permit_clean <- read_csv("~/JoeData/clean_data/permit_clean.csv")
+save(permit_clean, file = "intermediate data/permit_clean.rdata")
+
+load("intermediate data/permit_clean.rdata")
+
+sum(is.na(permit_clean$Zip.Code))
+
+lookup <- c(Vessel.ADFG.Number = "Vessel.ADFG", Batch.Year = "Year")
+permit_clean <- permit_clean %>% select(-"...1") %>% rename(all_of(lookup))
+permit.portfolio <- permit_clean %>% group_by(Batch.Year, Vessel.ADFG.Number) %>% summarise(permit.list = list(unique(Fishery)), permit.numbers = list(unique(Permit.Number)), permit.sequence = list(unique(Permit.Sequence)), num.entries = n(), max.permseq = max(Permit.Sequence)) #length of permit.list lists and permit.numbers list is equal for All observation
+
+salmon_permits <- permit_clean %>% select(Fishery) %>% filter(substr(Fishery,1,1)=="S") %>% distinct()
+permit.portfolio <- permit.portfolio %>% group_by(Vessel.ADFG.Number) %>% filter(any(salmon_permits$Fishery %in% permit.list), Vessel.ADFG.Number!=0) %>% mutate(Vessel.ADFG.Number=as.integer(Vessel.ADFG.Number))#limits to boats that have had at least one salmon permit in their lifetime
+#rm(permit_clean)
+
+
+#fishing.portfolio from catch data
+fishing.portfolio <- trip.revenue.fishery %>% group_by(Vessel.ADFG.Number, Batch.Year) %>% summarise(catch.portfolio = list(c(gsub(" ","",CFEC.Permit.Fishery))), catch.revenue = list(c(year.revenue)))#use gsub to remove spaces so that it matches with the permit data
+#join the permits
+permit.portfolio <- permit.portfolio %>% ungroup() %>% left_join(fishing.portfolio, join_by(Vessel.ADFG.Number, Batch.Year))
+permit.portfolio <- permit.portfolio %>% mutate(permit.fished = permit.list)
+
+
+
+test <- permit.portfolio <- permit.portfolio %>%
+  mutate(permit.fished = map2(permit.fished, catch.portfolio, ~ map(.x[[1]], ~ (.x %in% .y[[1]]))))
+
+get.permit.result <- function(df, permitname){
+  df <- df %>% group_by(Batch.Year, Vessel.ADFG.Number) %>% filter(any(permit.list %like% permitname)) %>% mutate(permit.fished = ifelse(catch.portfolio %like% permitname, TRUE, FALSE)) %>% ungroup()
+  return(df)
+}
+
+drift.gillnet <- salmon_permits %>% filter(Fishery %like% "03")
+drift.gillnet <- drift.gillnet$Fishery
+test <- get.permit.result(permit.portfolio, "S03T") %>% mutate(permit = "S03T")
+test <- test[0,]
+for (i in drift.gillnet){
+  temp <- get.permit.result(permit.portfolio, i) %>% mutate(permit = i)
+  test <- rbind(test,temp)
+}
+rm(temp)
+
+test %>% group_by(Batch.Year, permit) %>% 
+  summarise(ratio = sum(permit.fished)/n()) %>% 
+  filter(Batch.Year>1990 & Batch.Year<2022) %>% 
+  ggplot(aes(x=Batch.Year, y=ratio, color=permit)) +
+  geom_line()+
+  labs(title = "Proportion of Permits Fished: Drift Gillnet Salmon Fisheries", x=("Year"), y=("Proportion of Permits Fished That Recorded a Poditve Landing"))+
+  geom_line(size = 0.7)+
+  scale_color_brewer(palette = "Accent")
+
+test %>% group_by(Batch.Year, permit) %>% 
+  summarise(n.permitnoats = n()) %>% 
+  filter(Batch.Year>1990 & Batch.Year<2022) %>% 
+  ggplot(aes(x=Batch.Year, y=n.permitnoats, color=permit)) +
+  geom_line()+
+  labs(title = "Number of Unique Permited Boats: Drift Gillnet Salmon Fisheries", x=("Year"), y=("#boats"))+
+  geom_line(size = 0.7)+
+  scale_color_brewer(palette = "Accent")
+
+test %>% ungroup() %>% left_join %>% filter()%>%View()
+
+
+test %>% ungroup() %>% group_by(Batch.Year, permit) %>% summarise(mean.num.perm = mean(num.entries)) %>%
+  filter(Batch.Year>1990 & Batch.Year<2022) %>% 
+  ggplot(aes(x=Batch.Year, y=mean.num.perm, color=permit)) +
+  geom_line()+
+  labs(title = "Mean Number of Unique Permits of Boats: Drift Gillnet Salmon Fisheries", x=("Year"), y=("mean number of permits"))+
+  geom_line(size = 0.7)+
+  scale_color_brewer(palette = "Accent")
+
+test %>% ungroup() %>% group_by(Batch.Year, permit, Vessel.ADFG.Number)%>% mutate(unfished.permits = num.entries-length(catch.portfolio[[1]])) %>% 
+  ungroup() %>% group_by(Batch.Year, permit)%>% 
+  summarise(mean.unfished.perm = mean(unfished.permits)) %>%
+  filter(Batch.Year>1990 & Batch.Year<2022) %>% 
+  ggplot(aes(x=Batch.Year, y=mean.unfished.perm, color=permit)) +
+  geom_line()+
+  labs(title = "Mean Number of unfished permits: Drift Gillnet Salmon Fisheries", x=("Year"), y=("# of permits"))+
+  geom_line(size = 0.7)+
+  scale_color_brewer(palette = "Accent")
+
+
+trip.revenue.fishery %>% group_by(Batch.Year, CFEC.Permit.Fishery) %>% summarise(year.revenue = mean(year.revenue, na.rm = TRUE)) %>% 
+  filter(Batch.Year>1990 & Batch.Year<2022) %>% filter(gsub(" ","",CFEC.Permit.Fishery) %in% drift.gillnet) %>% 
+  ggplot(aes(x=Batch.Year, y=year.revenue, color=CFEC.Permit.Fishery)) +
+  geom_line()+
+  labs(title = "Mean boat revenue: Drift Gillnet Salmon Fisheries", x=("Year"), y=("$"))+
+  geom_line(size = 0.7)+
+  scale_color_brewer(palette = "Accent")
+#Then for each element look to see if that element is in the fishing portfolio list
+
+
+vessel_fishery_var %>% group_by(CFEC.Permit.Fishery) %>% summarise(year.revenue = mean(v.rev, na.rm = TRUE)) %>% filter(gsub(" ","",CFEC.Permit.Fishery) %in% drift.gillnet) %>% View()
+
+
+
 mypath <- "C:/Users/josep/Box Sync/Thesis/data/clean_data/permit_clean.csv"
 write.csv(df, mypath)
 df <- read.csv("C:/Users/josep/Box Sync/Thesis/data/clean_data/permit_clean.csv")
