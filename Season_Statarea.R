4f825699c8ef411151418d7d339dcba26042e8f3
Joe Raymond <72154886+Joseph-Raymond@users.noreply.github.com>
2025-06-19 16:02:56 -0700

    Create Season_Statarea.R

---
+++ Season_Statarea.R
@@ -0,0 +1,11 @@
+#First goal here is to get a basic summary of the statistical area variable and information from  stat area and find a way to export results to a gis software or plot them here 
+#interested in how long the vessel is out, distance from their landing port (also maybe home port), number of statistical areas fished if there are multiple
+#will need to get a baseline gis layer for statisitical fishing areas
+#https://www.arcgis.com/apps/View/index.html?appid=6552a4062d3d4e44a6163a5c848e0fad (groundfish)
+#https://alaska-department-of-fish-and-game-adfg.hub.arcgis.com/search (a bunch with the salmon districts) 
+#Second goal is to calculate the seasons for each fishery year (empirically using the dates of landings, i.e., it's a practical or realized season measure). Can then add a variable to the trip/fishers that describes when in the season they are fishing. These variables can include how many days you are into the season, percentage of time you are through the season, percentage of revenue that has been landed for the season
+#more aggregate measures include the season for each fishery-year, length of the season, change in the season. 
+#problem have to solve is seasons that span multiple years also if there are multiple seasons in a year
+
+#do statewide permits matter vs location restricted
+#enforcement having impacts
